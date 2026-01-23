be8ca25e19feca20a50da83035429b00f2e2d36d
Eshan Imesh <111144332+ImeshR@users.noreply.github.com>
2026-01-23 19:43:35 +0530

    VERCEL ANALYTICS ADD

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Results from "./pages/Results";
 import QuizPlay from "./pages/QuizPlay";
 import StudentProfile from "./pages/StudentProfile";
 import NotFound from "./pages/NotFound";
+import {Analytics} from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
@@ -44,6 +45,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <Analytics />
       </TooltipProvider>
     </AuthProvider>
   </QueryClientProvider>
